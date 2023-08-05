5b8ece8cc1df9a711c13f318b1f9c25638f0725a
Arundurai29 <114252340+Arundurai29@users.noreply.github.com>
2023-08-05 12:40:50 +0530

    Update active.js

--- assets/js/active.js
+++ assets/js/active.js
@@ -196,8 +196,11 @@ window.addEventListener('DOMContentLoaded', function() {
       prevEl: '.swiper-button-prev',
     },
     breakpoints: {
-      640: {
+      1300: {
         slidesPerView: 4,
+      },
+      640: {
+        slidesPerView: 3,
       },
       240: {
         slidesPerView: 1.1,
@@ -231,37 +234,37 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^]|\w)/g, "<span class='letter'>$&</span>");
+// var textWrapper = document.querySelector('.ml11 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/([^]|\w)/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml11 .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 1200
-  })
-  .add({
-    targets: '.ml11 .line',
-    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 20],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 100
-  }).add({
-    targets: '.ml11 .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i+1)
-  }).add({
-    targets: '.ml11',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml11 .line',
+//     scaleY: [0,1],
+//     opacity: [0.5,1],
+//     easing: "easeOutExpo",
+//     duration: 1200
+//   })
+//   .add({
+//     targets: '.ml11 .line',
+//     translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 20],
+//     easing: "easeOutExpo",
+//     duration: 700,
+//     delay: 100
+//   }).add({
+//     targets: '.ml11 .letter',
+//     opacity: [0,1],
+//     easing: "easeOutExpo",
+//     duration: 600,
+//     offset: '-=775',
+//     delay: (el, i) => 34 * (i+1)
+//   }).add({
+//     targets: '.ml11',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
 
 
     // :: 1.0 PRELOADER ACTIVE CODE
@@ -276,4 +279,3 @@ anime.timeline({loop: true})
           });
       }
     });
-  
